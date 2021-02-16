2a8317652be3808cc1b91665bf6ca4cf85ffab83
Artyom <artyom.sushkov@gmail.com>
2021-02-16 10:04:01 +0300

    added requirments.txt

--- ngrokbot/main.py
+++ ngrokbot/main.py
@@ -1,4 +1,3 @@
-import os
 import telebot
 from settings import TG_BOT_TOKEN
 from ngrokfuncs import get_ngrok_url
@@ -6,7 +5,7 @@ from ngrokfuncs import get_ngrok_url
 bot = telebot.TeleBot(TG_BOT_TOKEN)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['url'])
 def start_ngrok(message):
     ngrok_url = get_ngrok_url()
     bot.send_message(message.chat.id, ngrok_url)
